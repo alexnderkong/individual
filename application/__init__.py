16878cfcf1103d7cfed87c5a44d776d06ba3411d
alexnderkong <alexander.kong@academytrainee.com>
2019-11-07 09:40:16 +0000

    added mysql

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flask_db'
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = 'kXp2s5v8y/B?E(H+MbPeShVmYq3t6w9z'
